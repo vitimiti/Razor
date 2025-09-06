c63b4cc30389315af0c4ed702e8d1eeeb717a5d2
Victor Matia <vmatir@outlook.com>
2025-09-06 19:02:18 +0100

    Added more binary extensions
    Added extensions to the BinaryReader and BinaryWriter classes for 2 big-endian bytes.
    
    Signed-off-by: Victor Matia <vmatir@outlook.com>

--- Razor.Extensions/src/BinaryWriterExtensions.cs
+++ Razor.Extensions/src/BinaryWriterExtensions.cs
@@ -9,6 +9,18 @@ namespace Razor.Extensions;
 /// <summary>Extensions for the <see cref="BinaryWriter" /> class.</summary>
 public static class BinaryWriterExtensions
 {
+    /// <summary>Writes 2 bytes in big-endian form.</summary>
+    /// <param name="writer">
+    ///     The <see cref="BinaryWriter" /> to write to.
+    /// </param>
+    /// <param name="value">The value to write as 2 big-endian bytes.</param>
+    public static void WriteUInt16BigEndian(this BinaryWriter writer, ushort value)
+    {
+        Span<byte> bytes = stackalloc byte[2];
+        BinaryPrimitives.WriteUInt16BigEndian(bytes, value);
+        writer.Write(bytes);
+    }
+
     /// <summary>Writes 3 bytes in big-endian form.</summary>
     /// <param name="writer">
     ///     The <see cref="BinaryWriter" /> to write to.
