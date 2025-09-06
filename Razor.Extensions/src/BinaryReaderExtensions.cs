48f2b735e274272196f749ff6e7290614cc6b518
Victor Matia <vmatir@outlook.com>
2025-09-06 18:55:21 +0100

    Added binary reader/writer extensions
    Added extensions to allow reading/writing big-endian 3 and 4 bytes in the BinaryReader and BinaryWriter classes.
    
    Signed-off-by: Victor Matia <vmatir@outlook.com>

---
+++ Razor.Extensions/src/BinaryReaderExtensions.cs
@@ -0,0 +1,39 @@
+﻿// Licensed to the Razor contributors under one or more agreements.
+// The Razor project licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Buffers.Binary;
+
+namespace Razor.Extensions;
+
+/// <summary>Extensions for the <see cref="BinaryReader" /> class.</summary>
+public static class BinaryReaderExtensions
+{
+    /// <summary>Reads 3 bytes in big-endian form.</summary>
+    /// <param name="reader">
+    ///     The <see cref="BinaryReader" /> to read from.
+    /// </param>
+    /// <returns>
+    ///     A new <see cref="uint" /> with the 3 bytes in big-endian form.
+    /// </returns>
+    public static uint ReadUInt24BigEndian(this BinaryReader reader)
+    {
+        var byte1 = reader.ReadByte();
+        var byte2 = reader.ReadByte();
+        var byte3 = reader.ReadByte();
+        return (uint)((byte1 << 16) | (byte2 << 8) | byte3);
+    }
+
+    /// <summary>Reads 4 bytes in big-endian form.</summary>
+    /// <param name="reader">
+    ///     The <see cref="BinaryReader" /> to read from.
+    /// </param>
+    /// <returns>
+    ///     A new <see cref="uint" /> with the 4 bytes in big-endian form.
+    /// </returns>
+    public static uint ReadUInt32BigEndian(this BinaryReader reader)
+    {
+        var bytes = reader.ReadBytes(4);
+        return BinaryPrimitives.ReadUInt32BigEndian(bytes);
+    }
+}
---
+++ Razor.Extensions/src/BinaryWriterExtensions.cs
@@ -0,0 +1,35 @@
+﻿// Licensed to the Razor contributors under one or more agreements.
+// The Razor project licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Buffers.Binary;
+
+namespace Razor.Extensions;
+
+/// <summary>Extensions for the <see cref="BinaryWriter" /> class.</summary>
+public static class BinaryWriterExtensions
+{
+    /// <summary>Writes 3 bytes in big-endian form.</summary>
+    /// <param name="writer">
+    ///     The <see cref="BinaryWriter" /> to write to.
+    /// </param>
+    /// <param name="value">The value to write as 3 big-endian bytes.</param>
+    public static void WriteUInt24BigEndian(this BinaryWriter writer, uint value)
+    {
+        writer.Write((byte)(value >> 16));
+        writer.Write((byte)(value >> 8));
+        writer.Write((byte)value);
+    }
+
+    /// <summary>Writes 4 bytes in big-endian form.</summary>
+    /// <param name="writer">
+    ///     The <see cref="BinaryWriter" /> to write to.
+    /// </param>
+    /// <param name="value">The value to write as 4 big-endian bytes.</param>
+    public static void WriteUInt32BigEndian(this BinaryWriter writer, uint value)
+    {
+        Span<byte> bytes = stackalloc byte[4];
+        BinaryPrimitives.WriteUInt32BigEndian(bytes, value);
+        writer.Write(bytes);
+    }
+}
