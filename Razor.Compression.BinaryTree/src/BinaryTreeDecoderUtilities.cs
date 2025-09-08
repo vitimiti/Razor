cf76fafb1a735c29e3f609c1b73c6293ac68f69e
Victor Matia <vmatir@outlook.com>
2025-09-08 20:36:36 +0100

    Added missing license notice
    Added the notice header notice that was missing.
    
    Signed-off-by: Victor Matia <vmatir@outlook.com>

--- Razor.Compression.BinaryTree/src/BinaryTreeDecoderUtilities.cs
+++ Razor.Compression.BinaryTree/src/BinaryTreeDecoderUtilities.cs
@@ -1,3 +1,7 @@
+// Licensed to the Razor contributors under one or more agreements.
+// The Razor project licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
 using Razor.Extensions;
 
 namespace Razor.Compression.BinaryTree;
