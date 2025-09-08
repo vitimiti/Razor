671fbc273d962367cd4ba297742b582c57ddda31
Victor Matia <vmatir@outlook.com>
2025-09-08 07:38:47 +0100

    Created decoding utilities
    Created utilities to aid in the decoding of BinaryTree data.
    
    Signed-off-by: Victor Matia <vmatir@outlook.com>

---
+++ Razor.Compression.BinaryTree/src/BinaryTreeDecoderUtilities.cs
@@ -0,0 +1,41 @@
+using Razor.Extensions;
+
+namespace Razor.Compression.BinaryTree;
+
+internal static class BinaryTreeDecoderUtilities
+{
+    public static bool IsBinaryTreeCompressed(Stream stream)
+    {
+        if (stream.Length < 2)
+        {
+            return false;
+        }
+
+        stream.Position = 0;
+        using BinaryReader reader = new(stream, EncodingExtensions.Ansi, leaveOpen: true);
+        var packType = reader.ReadUInt16BigEndian();
+        return packType is 0x46FB or 0x47FB;
+    }
+
+    public static uint GetUncompressedSize(Stream stream)
+    {
+        if (!IsBinaryTreeCompressed(stream))
+        {
+            throw new ArgumentException(
+                "The stream is not BinaryTree compressed data.",
+                nameof(stream)
+            );
+        }
+
+        ArgumentOutOfRangeException.ThrowIfLessThan(stream.Length, 2);
+
+        stream.Position = 0;
+        using BinaryReader reader = new(stream, EncodingExtensions.Ansi, leaveOpen: true);
+        var packType = reader.ReadUInt16BigEndian();
+        var offset = packType is 0x46FB ? 2 : 5;
+
+        // Create an offset
+        _ = reader.ReadBytes(offset);
+        return reader.ReadUInt24BigEndian();
+    }
+}
