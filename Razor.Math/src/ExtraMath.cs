1e29ea3f274fd9e683cd922d3140f3bb0577240f
Victor Matia <vmatir@outlook.com>
2025-09-12 16:31:57 +0100

    Made a faster InvSqrt
    Now the InvSqrt uses the Quake III's Newton-Rhapson approximation by default.
    
    Signed-off-by: Victor Matia <vmatir@outlook.com>

--- Razor.Math/src/ExtraMath.cs
+++ Razor.Math/src/ExtraMath.cs
@@ -9,9 +9,40 @@ namespace Razor.Math;
 [PublicAPI]
 public static class ExtraMath
 {
-    public static float InvSqrt(float value)
+    public static float InvSqrt(float value, bool fast = true)
     {
-        return 1F / float.Sqrt(value);
+        if (!fast)
+        {
+            return 1F / float.Sqrt(value);
+        }
+
+        switch (value)
+        {
+            // Match IEEE behavior for special cases
+            case < float.Epsilon: // Basically 0F
+                return float.PositiveInfinity; // 1/sqrt(0) -> +Inf
+            case < 0f or float.NaN:
+                return float.NaN;
+        }
+
+        if (float.IsPositiveInfinity(value))
+        {
+            return 0f; // 1/sqrt(Inf) -> 0
+        }
+
+        // Quake III fast inverse square root (no assembly)
+        // Uses bit-level hack + two Newton-Raphson iterations for accuracy
+        var x2 = value * 0.5f;
+        var i = BitConverter.SingleToInt32Bits(value);
+        i = 0x5F3759DF - (i >> 1);
+        var y = BitConverter.Int32BitsToSingle(i);
+
+        // 1st Newton-Raphson iteration
+        y *= (1.5f - (x2 * y * y));
+        // 2nd iteration: improves precision to near full float accuracy
+        y *= (1.5f - (x2 * y * y));
+
+        return y;
     }
 
     public static bool IsValid(float value)
