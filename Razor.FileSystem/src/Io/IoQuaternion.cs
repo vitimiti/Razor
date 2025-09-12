f58741098f5babf7ea3b3b4ca1371ec350ac4ba7
Victor Matia <vmatir@outlook.com>
2025-09-12 17:22:08 +0100

    Created the basic IO classes
    Created basic structures for the the IO system.
    
    Signed-off-by: Victor Matia <vmatir@outlook.com>

---
+++ Razor.FileSystem/src/Io/IoQuaternion.cs
@@ -0,0 +1,13 @@
+﻿// Licensed to the Razor contributors under one or more agreements.
+// The Razor project licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using JetBrains.Annotations;
+
+namespace Razor.FileSystem.Io;
+
+[PublicAPI]
+public record IoQuaternion()
+{
+    public float[] Q { get; init; } = new float[4];
+}
---
+++ Razor.FileSystem/src/Io/IoVector3.cs
@@ -0,0 +1,10 @@
+﻿// Licensed to the Razor contributors under one or more agreements.
+// The Razor project licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using JetBrains.Annotations;
+
+namespace Razor.FileSystem.Io;
+
+[PublicAPI]
+public record IoVector3(float X, float Y, float Z);
