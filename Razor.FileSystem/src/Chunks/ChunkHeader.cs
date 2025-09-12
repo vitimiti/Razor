e6c173bd351962bfd176b0a58686f7da76aaceb3
Victor Matia <vmatir@outlook.com>
2025-09-12 17:29:17 +0100

    Added the ChunkHeader class
    Added a class to create chunk headers.
    
    Signed-off-by: Victor Matia <vmatir@outlook.com>

---
+++ Razor.FileSystem/src/Chunks/ChunkHeader.cs
@@ -0,0 +1,39 @@
+﻿// Licensed to the Razor contributors under one or more agreements.
+// The Razor project licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using JetBrains.Annotations;
+
+namespace Razor.FileSystem.Chunks;
+
+[PublicAPI]
+public class ChunkHeader(uint chunkTypeFlags, uint chunkSize)
+{
+    private uint _chunkSize = chunkSize;
+
+    public uint ChunkTypeFlags { get; set; } = chunkTypeFlags;
+
+    public uint ChunkSize
+    {
+        get => _chunkSize & 0x7FFFFFFF;
+        set
+        {
+            _chunkSize &= 0x80000000;
+            _chunkSize |= value & 0x7FFFFFFF;
+        }
+    }
+
+    public bool ContainsSubChunks
+    {
+        get => (_chunkSize & 0x80000000) != 0;
+        set => _chunkSize = value ? _chunkSize | 0x80000000 : _chunkSize & 0x7FFFFFFF;
+    }
+
+    public ChunkHeader()
+        : this(0, 0) { }
+
+    public void AddSize(uint size)
+    {
+        ChunkSize += size;
+    }
+}
