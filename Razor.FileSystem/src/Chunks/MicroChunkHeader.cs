3dbb204557a42941eb1ee81614657dad0735036e
Victor Matia <vmatir@outlook.com>
2025-09-12 17:32:25 +0100

    Added the MicroChunkHeader class
    Added a class to manage microchunk headers.
    
    Signed-off-by: Victor Matia <vmatir@outlook.com>

---
+++ Razor.FileSystem/src/Chunks/MicroChunkHeader.cs
@@ -0,0 +1,22 @@
+﻿// Licensed to the Razor contributors under one or more agreements.
+// The Razor project licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using JetBrains.Annotations;
+
+namespace Razor.FileSystem.Chunks;
+
+[PublicAPI]
+public class MicroChunkHeader(byte chunkType, uint chunkSize)
+{
+    public byte ChunkType { get; set; } = chunkType;
+    public uint ChunkSize { get; set; } = chunkSize;
+
+    public MicroChunkHeader()
+        : this(0, 0) { }
+
+    public void AddSize(uint size)
+    {
+        ChunkSize += size;
+    }
+}
